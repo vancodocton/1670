a64c99fae5b8f60cf4d44b2f483e9326096e5b4e
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-08 00:47:23 +0700

    Create Admin Controller and View

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApp.Models;
+using WebApp.Utils;
+
+namespace WebApp.Controllers
+{
+    [Authorize(Roles = Role.Admin)]
+    public class AdminController : Controller
+    {
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
