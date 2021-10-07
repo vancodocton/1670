2bf312fc67bd87fb6c228e620f5259b5f9113ee5
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-08 00:12:12 +0700

    Add Role.cs

---
+++ WebApp/Utils/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Utils
+{
+    public class Role
+    {
+        public string Admin = "Admin";
+        public string Staff = "Staff";
+        public string Trainer = "Trainer";
+        public string Trainee = "Trainee";
+    }
+}
