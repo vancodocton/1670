6c2aa8224af0e876ae4979158fbd42813c47bef4
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-10 03:26:24 +0700

    Move staff controller to Staff Area

---
+++ WebApp/Areas/Staff/Controllers/CourseCategoryController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.Utils;
+
+namespace WebApp.Areas.Staff.Controllers
+{
+    [Authorize(Roles = Role.Staff)]
+    public class CourseCategoryController : Controller
+    { 
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var categories = await _context.CourseCategories.ToListAsync();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CourseCategory model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                _context.CourseCategories.Add(model);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                ViewBag.Message = e.InnerException;
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(CourseCategory model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                _context.CourseCategories.Add(model);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                ViewBag.Message = e.InnerException;
+                return View(model);
+            }
+
+            return RedirectToAction(nameof(CourseCategory));
+        }
+    }
+
+}
---
+++ WebApp/Areas/Staff/StaffAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Areas.Staff
+{
+    public class StaffAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Staff";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Staff_default",
+                "Staff/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebApp.Areas.Staff.Controllers" }
+            );
+        }
+    }
+}
