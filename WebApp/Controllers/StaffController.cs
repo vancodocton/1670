df5e3ba005dc56d734f21e7828f4726afc7d76f7
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-09 20:46:52 +0700

    Add controller for staff

---
+++ WebApp/Controllers/StaffController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using WebApp.Utils;
+
+namespace WebApp.Controllers
+{
+    [Authorize(Roles = Role.Staff)]
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
