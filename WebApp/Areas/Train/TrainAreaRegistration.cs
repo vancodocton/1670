4f85faf2306f5bde7bb3b77bf6ef8e8afb930eda
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-10 21:40:43 +0700

    Add train area for trainer and trainee

---
+++ WebApp/Areas/Train/TrainAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Areas.Train
+{
+    public class TrainAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Train";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Train_default",
+                "Train/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional, },
+                new[] { "WebApp.Areas.Train.Controllers" }
+            );
+        }
+    }
+}
