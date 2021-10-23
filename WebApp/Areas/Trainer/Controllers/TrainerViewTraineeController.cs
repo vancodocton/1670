7d1e3edc0bece40af0eb934f6e8530f0f236012a
Thien-Pham25 <pdthienvn@gmail.com>
2021-10-23 10:53:26 +0700

    add trainer Area (unfinished)

---
+++ WebApp/Areas/Trainer/Controllers/TrainerViewTraineeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Areas.Trainer.Controllers
+{
+    public class TrainerViewTraineeController : Controller
+    {
+        // GET: Trainer/TrainerViewTrainee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
