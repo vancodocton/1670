ddfddaa26491e76a8dfe70057753700e6182f25f
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2021-10-26 00:36:59 +0700

    Formating controller code and design views of couse management in Staff side

---
+++ WebApp/ViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    [NotMapped]
+    public class CreateCourseViewModel
+    {
+        public Course Course { get; set; }
+        public SelectList Categories { get; set; }
+    }
+}
