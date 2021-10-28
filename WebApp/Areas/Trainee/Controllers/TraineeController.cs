76a672e7a787b06069d6dd0759710ea9166c078c
Thien-Pham25 <pdthienvn@gmail.com>
2021-10-28 14:51:15 +0700

    add trainee

--- WebApp/Areas/Trainee/Controllers/TraineeController.cs
+++ WebApp/Areas/Trainee/Controllers/TraineeController.cs
@@ -16,8 +16,33 @@ namespace WebApp.Areas.Trainee.Controllers
         public ActionResult Index()
         {
             string traineeId = User.Identity.GetUserId();
+            var trainee = _context.Users.SingleOrDefault(t => t.Id == traineeId);
+            var courses = _context.Courses
+                .Include(t => t.CourseCategory)
+                .Where(t => t.Trainees.Any(c => c.UserId == traineeId))
+                .ToList();
+            return View(courses);
+        }
+        public ActionResult ViewTrainee(int courseId)
+        {
+            var course = _context.Courses.SingleOrDefault(c => c.Id == courseId);
+            var trainees = _context.Trainees
+                .Include(t => t.User)
+                .Where(t => t.Courses.Any(c => c.Id == courseId))
+                .ToList();
+            var mine = trainees.Single(t => t.UserId == User.Identity.GetUserId());
+            trainees.Remove(mine);
+            for(int i = 0; i < trainees.Count; i++)
+            {
+                if(trainees[i].UserId == User.Identity.GetUserId())
+                {
+                    trainees.RemoveAt(i);
+                    break;
+                }
+                    
+            }
 
-            return View();
+            return View(trainees);
         }
     }
 }
